e9fb62f35d01838b008d79232e15df60c9939036
weiyafei <2553241022@qq.com>
2023-02-15 02:19:52 +0800

    express router

--- packages/express/app.ts
+++ packages/express/app.ts
@@ -2,6 +2,8 @@ import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
 
+import router from './router'
+
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -10,14 +12,7 @@ app.use(cors())
 
 const PORT = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World! get')
-})
-
-app.post('/', (req, res) => {
-  console.log(req.body)
-  res.send('Hello World! post')
-})
+app.use('/api', router)
 
 app.listen(PORT, () => {
   console.info(`Server running on port ${PORT}`)
