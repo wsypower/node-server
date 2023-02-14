9a28c9c5d37f0e5e1684e194e6d6f6585aae40fe
weiyafei <2553241022@qq.com>
2023-02-15 02:15:42 +0800

    添加express 依赖

--- packages/express/app.ts
+++ packages/express/app.ts
@@ -1,11 +1,22 @@
 import express from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
 
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(morgan('dev'))
+app.use(cors())
 
 const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! get')
+})
+
+app.post('/', (req, res) => {
+  console.log(req.body)
+  res.send('Hello World! post')
 })
 
 app.listen(PORT, () => {
